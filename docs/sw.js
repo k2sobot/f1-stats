1f664fac8cea44bbe262d149c3e7218794af6a4b
K2SO <k2so@arctek.co.za>
2026-08-03 11:08:23 +0200

    Add PWA install functionality
    - Add manifest.json for PWA support
    - Add service worker for offline capability
    - Add install button in header
    - Add app icons (192x192 and 512x512)
    - Add iOS web app meta tags

---
+++ docs/sw.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = 'f1-stats-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/assets/css/style.css',
+  '/assets/js/app.js',
+  '/assets/favicon.svg'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(ASSETS_TO_CACHE))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name))
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
